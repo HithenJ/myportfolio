c4072186c434fab2c20584a41ad1388adeca2640
HithenJ <hithenjessu@gmail.com>
2025-07-27 18:38:34 +0530

    updated code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
 import { AboutComponent } from './component/about/about.component';
 import { SkillsComponent } from './component/skills/skills.component';
+import { ProjectsComponent } from './component/projects/projects.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
     {path:'about',component:AboutComponent},
-        {path:'skills',component:SkillsComponent},
+        {path:'Skills',component:SkillsComponent},
+                {path:'Project',component:ProjectsComponent},
+                                {path:'Contact-Us',component:ContactComponent},
+
+
 
 
   {path:'**',component:NotfoundComponent},
--- src/app/component/hero-section/hero-section.component.ts
+++ src/app/component/hero-section/hero-section.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-section.component.css']
 })
 export class HeroSectionComponent implements OnInit {
-
+  words: string[] = ['Front End Development', 'Back End Development'];
+  displayText: string = '';
   constructor() { }
 
+   private wordIndex = 0;
+  private charIndex = 0;
+  private isDeleting = false;
+
   ngOnInit(): void {
+    this.typeEffect();
   }
 
+  typeEffect() {
+    const currentWord = this.words[this.wordIndex];
+    
+    if (!this.isDeleting && this.charIndex <= currentWord.length) {
+      this.displayText = currentWord.substring(0, this.charIndex++);
+    } else if (this.isDeleting && this.charIndex >= 0) {
+      this.displayText = currentWord.substring(0, this.charIndex--);
+    }
+
+    if (this.charIndex === currentWord.length + 1) {
+      this.isDeleting = true;
+      setTimeout(() => this.typeEffect(), 1000); // pause at full word
+      return;
+    } else if (this.charIndex === -1) {
+      this.isDeleting = false;
+      this.wordIndex = (this.wordIndex + 1) % this.words.length;
+    }
+
+    setTimeout(() => this.typeEffect(), this.isDeleting ? 60 : 100);
+  }
 }
+
